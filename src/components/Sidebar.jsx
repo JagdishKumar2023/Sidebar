eaecf5c390b637950f616435179c54e873305b42
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-11-04 23:58:32 +0530

    Sidebar is complete 90%

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,8 +4,9 @@ import { FaBars, FaHome, FaUser } from "react-icons/fa";
 import { MdMessage } from "react-icons/md";
 import { BiAnalyse, BiSearch } from "react-icons/bi";
 import { GoFileSubmodule } from "react-icons/go";
-import { BsCartCheck } from "react-icons/bs";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { FiShoppingCart } from "react-icons/fi";
+import { useState } from "react";
 
 const routes = [
   {
@@ -36,38 +37,118 @@ const routes = [
   {
     path: "/order",
     name: "Order",
-    icon: <BsCartCheck />,
+    icon: <FiShoppingCart />,
   },
 ];
 
 const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  const inputAnimation = {
+    hidden: {
+      width: 0,
+      padding: 0,
+      opacity: 0,
+    },
+    show: {
+      width: "140px",
+      padding: "5px 15px",
+      opacity: 1,
+      transition: {
+        duration: 0.7,
+      },
+    },
+  };
+
+  const showAnimation = {
+    hidden: {
+      opacity: 0,
+    },
+    show: {
+      opacity: 1,
+      transition: {
+        duration: 0.7,
+      },
+    },
+  };
+
   return (
     <div className="mainContainer">
       <motion.div
-        initial={{ width: "0px" }}
-        animate={{ width: "200px" }}
-        transition={{ duration: 0.5, ease: "easeInOut" }}
+        initial={{ width: "35px" }}
+        animate={{ width: isOpen ? "200px" : "35px" }}
+        transition={{
+          duration: 0.5,
+          ease: "easeInOut",
+          damping: 12,
+          type: "spring",
+        }}
         className="sidebar"
       >
         <div className="top_section">
-          <h1 className="logo">Dashboard</h1>
+          {isOpen && (
+            <motion.h1
+              className="logo"
+              variants={showAnimation}
+              initial="hidden"
+              animate="show"
+            >
+              Dashboard
+            </motion.h1>
+          )}
           <div className="bars">
-            <FaBars />
+            <FaBars onClick={toggle} />
           </div>
         </div>
         <div className="search">
           <div className="search_icon">
             <BiSearch />
           </div>
-          <input placeholder="Search..." />
+          <AnimatePresence>
+            {isOpen && (
+              <motion.input
+                placeholder="Search..."
+                variants={inputAnimation}
+                initial="hidden"
+                animate="show"
+                exit="hidden"
+              />
+            )}
+          </AnimatePresence>
         </div>
         <section className="routes">
-          {routes.map((route) => (
-            <NavLink to={route.path} key={route.name}>
-              <div className="icon">{route.icon}</div>
-              <div className="link_text">{route.name}</div>
-            </NavLink>
-          ))}
+          <AnimatePresence>
+            {isOpen &&
+              routes.map((route) => (
+                <NavLink
+                  activeClassName="active"
+                  to={route.path}
+                  key={route.name}
+                  className="link"
+                >
+                  <motion.div
+                    className="icon"
+                    variants={showAnimation}
+                    initial="hidden"
+                    animate="show"
+                    exit="hidden"
+                  >
+                    {route.icon}
+                  </motion.div>
+                  <motion.div
+                    className="link_text"
+                    variants={showAnimation}
+                    initial="hidden"
+                    animate="show"
+                    exit="hidden"
+                  >
+                    {route.name}
+                  </motion.div>
+                </NavLink>
+              ))}
+          </AnimatePresence>
         </section>
       </motion.div>
       <main>{children}</main>
